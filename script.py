50ad0e5ef2ce20a781a15a39d7c608a7c7fa664a
Edward Ellensohn <edwardellensohn2018@u.northwestern.edu>
2016-06-05 12:15:47 -0500

    kaggle things

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+
 # Import the linear regression class
 from sklearn.linear_model import LinearRegression
 # Sklearn also has a helper that makes it easy to do cross validation
@@ -57,4 +58,151 @@ predictions = np.concatenate(predictions, axis=0)
 predictions[predictions > .5] = 1
 predictions[predictions <=.5] = 0
 accuracy = sum(predictions[predictions == titanic["Survived"]]) / len(predictions)
-print(accuracy)
+
+print('-------------------\n accuracy: {0:.4f} \n-------------------'.format(accuracy))
+
+
+
+
+
+######Going to try logistic Regression##########3
+from sklearn import cross_validation
+from sklearn.linear_model import LogisticRegression
+
+#initialize the algorithm
+alg = LogisticRegression(random_state=1) #rand num seed generation?
+
+# Compute the accuracy score for all the cross validation folds.  (much simpler than what we did before!)
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=4)
+# Take the mean of the scores (because we have one for each fold)
+print(scores.mean())
+
+
+
+####### Random Forest #############
+print('\n\n --------------try random forest----------')
+from sklearn.ensemble import RandomForestClassifier
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked"]
+
+# Initialize our algorithm with the default paramters
+# n_estimators is the number of trees we want to make
+# min_samples_split is the minimum number of rows we need to make a split
+# min_samples_leaf is the minimum number of samples we can have at the place where a tree branch ends (the bottom points of the tree)
+alg = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split=4, min_samples_leaf=2)
+#perform cross validation and report accuracy
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv = 3)
+print(scores.mean())
+
+#######Generating New Features############3
+print('\n\ngenerate title feature:')
+# Generating a familysize column
+titanic["FamilySize"] = titanic["SibSp"] + titanic["Parch"]
+
+# The .apply method generates a new series
+titanic["NameLength"] = titanic["Name"].apply(lambda x: len(x))
+
+### Using the Title of the person##########
+import re
+
+# A function to get the title from a name.
+def get_title(name):
+    # Use a regular expression to search for a title.  Titles always consist of capital and lowercase letters, and end with a period.
+    title_search = re.search(' ([A-Za-z]+)\.', name)
+    # If the title exists, extract and return it.
+    if title_search:
+        return title_search.group(1)
+    return ""
+
+# Get all the titles and print how often each one occurs.
+titles = titanic["Name"].apply(get_title)
+print(pd.value_counts(titles))
+
+# Map each title to an integer.  Some titles are very rare, and are compressed into the same codes as other titles.
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Dr": 5, "Rev": 6, "Major": 7, "Col": 7, "Mlle": 8, "Mme": 8, "Don": 9, "Lady": 10, "Countess": 10, "Jonkheer": 10, "Sir": 9, "Capt": 7, "Ms": 2}
+for k,v in title_mapping.items():
+    titles[titles == k] = v
+
+# Verify that we converted everything.
+print(pd.value_counts(titles))
+
+# Add in the title column.
+titanic["Title"] = titles
+
+#Feature Selection ##############333
+print('\n\nUse the feature seection stuff from sklearn:')
+from sklearn.feature_selection import SelectKBest, f_classif
+
+predictors = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", "Embarked", "FamilySize", "Title", "FamilyId"]
+
+# Perform feature selection
+selector = SelectKBest(f_classif, k=5)
+selector.fit(titanic[predictors], titanic["Survived"])
+
+# Get the raw p-values for each feature, and transform from p-values into scores
+scores = -np.log10(selector.pvalues_)
+
+# Plot the scores.  See how "Pclass", "Sex", "Title", and "Fare" are the best?
+plt.bar(range(len(predictors)), scores)
+plt.xticks(range(len(predictors)), predictors, rotation='vertical')
+plt.show()
+
+# Pick only the four best features.
+predictors = ["Pclass", "Sex", "Fare", "Title"]
+
+alg = RandomForestClassifier(random_state=1, n_estimators=150, min_samples_split=8, min_samples_leaf=4)
+
+scores = cross_validation.cross_val_score(alg, titanic[predictors], titanic["Survived"], cv=3)
+print(scores.mean())
+
+
+
+##Try networkx
+
+#This part is just me messing around with representing
+#the data as a graph
+
+import networkx as nx
+graph = nx.Graph()
+print(titanic)
+print('\n\n\n\n\n\n')
+print(titanic["PassengerId"])
+# for i in titanic:
+# 	graph.add_node(titanic["PassengerId"])
+graph.add_node('Death Node')
+graph.add_node('Sex')
+for idx, row in titanic.iterrows():
+    graph.add_node(idx)
+    if titanic["Survived"][idx] == 1:
+    	graph.add_edge(idx, 'Death Node')
+    if titanic["Sex"][idx] == 1:
+    	graph.add_edge(idx, 'Sex')
+
+
+
+import matplotlib.pyplot as plt
+from matplotlib import pylab
+
+def save_graph(graph,file_name):
+    #initialze Figure
+    plt.figure(num=None, figsize=(20, 20), dpi=80)
+    plt.axis('off')
+    fig = plt.figure(1)
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph,pos)
+    nx.draw_networkx_edges(graph,pos)
+    nx.draw_networkx_labels(graph,pos)
+
+    cut = 1.00
+    xmax = cut * max(xx for xx, yy in pos.values())
+    ymax = cut * max(yy for xx, yy in pos.values())
+    plt.xlim(0, xmax)
+    plt.ylim(0, ymax)
+
+    plt.savefig(file_name,bbox_inches="tight")
+    pylab.close()
+    del fig
+
+#Assuming that the graph g has nodes and edges entered
+#save_graph(graph,"my_graph.pdf")
+nx.draw(graph)
+plt.savefig("try.png")
